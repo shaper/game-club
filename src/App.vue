631f58fea671619c5e4c1f1cce42f1ac74bb97c3
Walter Korman <lemurware@gmail.com>
2023-04-18 08:41:32 -0700

    Trivial Vue app for game club history.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import GameLog from './components/GameLog.vue'
+</script>
+
+<template>
+  <main>
+    <GameLog />
+  </main>
+</template>
---
+++ src/components/GameLog.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GameEntry from './GameEntry.vue'
+import meetings from '../assets/HISTORY.json'
+</script>
+
+<template>
+  <h1>Video Game Club Meeting History</h1>
+  <div v-for="meet in meetings">
+    <GameEntry v-bind="meet"></GameEntry>
+  </div>
+</template>
