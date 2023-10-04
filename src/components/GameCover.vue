56f3d690489f91d5d04b862f11184c09f06c6f8e
Aaron Gable <aaron@aarongable.com>
2023-10-04 15:53:17 -0700

    Place all titles to the left of all covers (#13)
    Refactor the MeetingEntry to separately list the game titles
    and the game covers, rather than putting the title directly above
    each cover. This allows long titles to more elegantly overflow,
    without changing the spacing around the game covers. Also, give
    the covers some horizontal scrolling behavior, for viewers on mobile.

---
+++ src/components/GameCover.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String,
+  url: String,
+  steamAppId: String,
+  coverArt: String,
+  playtime: Number,
+});
+
+const roundPlaytime = (time: number): number => {
+  return Math.round(time * 10) / 10;
+};
+</script>
+
+<template>
+  <div>
+    <div v-if="coverArt">
+      <img :src="coverArt" />
+    </div>
+    <div v-else-if="steamAppId">
+      <a :href="`https://store.steampowered.com/app/${steamAppId}/`">
+        <img :src="`https://steamcdn-a.akamaihd.net/steam/apps/${steamAppId}/library_600x900.jpg`" />
+      </a>
+    </div>
+    <div v-else>
+      <img src="../assets/missing.svg" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 200px;
+  border-radius: 5px;
+  margin: 0px 10px 10px 0px;
+  box-shadow: 5px 5px 5px 0px rgba(84, 84, 84, 0.65);
+}
+</style>
---
+++ src/components/GameTitle.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String,
+  url: String,
+  steamAppId: String,
+  coverArt: String,
+  playtime: Number,
+});
+
+const roundPlaytime = (time: number): number => {
+  return Math.round(time * 10) / 10;
+};
+</script>
+
+<template>
+  <span v-if="playtime" class="playtime">
+      {{ roundPlaytime(playtime) }}
+  </span>
+  <span v-if="url">
+    <a :href="url">{{ name }}</a>
+  </span>
+  <span v-else>
+    {{ name }}
+  </span>
+</template>
+
+<style scoped>
+.playtime {
+  color: #808080;
+  padding-right: 1em;
+}
+.playtime::before {
+  content: "🕑";
+  font-size: 85%;
+}
+</style>
